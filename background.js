9aae3b05dd4bb6102b00d3b344d50a21aa3ad94a
rsanek <rsanek@gmail.com>
2020-12-08 20:11:47 -0800

    fix perms for ffx

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.url.match(/zdone.co/)) {
-    chrome.pageAction.show(tabId);
-  } else {
-    chrome.pageAction.hide(tabId);
-  }
-});
