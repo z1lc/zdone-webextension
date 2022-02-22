a9d6c3da27817d7276f865b3b58d5fbd5a531e17
z1lc <rsanek@gmail.com>
2022-02-21 19:35:20 -0800

    Implement ability to send highlights to Readwise

--- follow.js
+++ follow.js
@@ -1,7 +1,7 @@
 chrome.storage.sync.get("apiKey", function (items) {
   if (items.apiKey.length !== 36) {
     alert("zdone-webextension API key is invalid. Please set it by visiting zdone.co and clicking the extension.")
-  } else {
+  } else if (window.location.href.includes("en.wikipedia.org")) {
     let pageName = window.location.href.split('/').pop();
     $.get(`https://www.zdone.co/api/${items.apiKey}/follow/${pageName}/`, function (data) {
       let button = document.createElement("button");
@@ -31,5 +31,46 @@ chrome.storage.sync.get("apiKey", function (items) {
       button.style.cssText = css;
       document.body.appendChild(button);
     });
+  }
+})
+
+chrome.storage.sync.get("readwiseAccessToken", function (items) {
+  if (items.readwiseAccessToken.length !== 50) {
+    alert("The Readwise Access Token is invalid. Please set it by clicking the extension.")
+  } else {
+    document.onkeyup = function (event) {
+    if (event.key.toLowerCase() === 'a') {
+      let maybe_selection = window.getSelection().toString();
+      if (maybe_selection !== "") {
+        $
+          .ajax({
+            contentType: "application/json",
+            url: `https://readwise.io/api/v2/highlights/`,
+            type: "POST",
+            headers: {
+              "Authorization": `Token ${items.readwiseAccessToken}`,
+            },
+            data: JSON.stringify({
+              "highlights": [{
+                text: maybe_selection,
+                title: document.title,
+                source_url: window.location.href,
+                source_type: 'zdone-webextension',
+                category: 'articles',
+                location: (Date.now() / 1000).toFixed(),
+                location_type: 'time_offset',
+                highlighted_at: new Date().toISOString(),
+              }]
+            })
+          })
+          .fail(function (xhr, status, error) {
+            alert(`Received error response: ${xhr.responseText}`);
+          })
+          .done(function () {
+            window.getSelection().removeAllRanges();
+          })
+      }
+    }
+  }
   }
 })
--- options.js
+++ options.js
@@ -1,21 +1,25 @@
 // Saves options to chrome.storage
 function save_options() {
-  var apiKey = document.getElementById('api_key').value;
+  let apiKey = document.getElementById('api_key').value;
+  let readwiseAccessToken = document.getElementById('readwise_access_token').value;
   chrome.storage.sync.set({
     apiKey: apiKey,
+    readwiseAccessToken: readwiseAccessToken
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
+// Restores state using the preferences stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     apiKey: '',
+    readwiseAccessToken: '',
   }, function (items) {
     document.getElementById('api_key').value = items.apiKey;
+    document.getElementById('readwise_access_token').value = items.readwiseAccessToken;
   });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('api_key').addEventListener('change', save_options);
+document.getElementById('readwise_access_token').addEventListener('change', save_options);
